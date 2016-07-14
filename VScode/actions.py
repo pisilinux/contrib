ccd7bb76201a2b2bdfece57162ee0b7c41551fc4
Sami Babat <sami.babat@gmail.com>
2016-07-14 14:54:24 +0300

    Update actions.py

--- VScode/actions.py
+++ VScode/actions.py
@@ -2,19 +2,12 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
-def setup():
-    autotools.configure()
-
-def build():
-    autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "NEWS", "README")
+    pisitools.insinto("/usr/share/VSCode-linux-x64, "./*")   
