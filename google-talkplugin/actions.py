2b17369264b182632993df4d959bc088f24fe701
groni <groni@pisilinux.org>
2016-11-10 16:14:45 +0100

    google-talkplugin version bump

--- google-talkplugin/actions.py
+++ google-talkplugin/actions.py
@@ -8,32 +8,19 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "%s" % get.ARCH()
+WorkDir = "x86_64"
+
+NoStrip = ["/opt", "/usr"]
+IgnoreAutodep = True
+
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf google-talkplugin_5.41.0.0-1_amd64.deb")
+    shelltools.system("tar xvf data.tar.*")
 
 def install():
-    # Define bits variable for 32-bit and 64-bit architectures
-    bits = "amd64" if get.ARCH() == "x86_64" else "i386"
-
-    # Extract Debian package
-    shelltools.system("ar -xv google-talkplugin_current_%s.deb" % bits)
-
-    # Extract data.tar.gz, we use some exclude parameters here
-    # because otherwise we get fchownat related sandbox errors
-    shelltools.system("tar -xzvf data.tar.gz --exclude 'usr/lib/*' --exclude 'usr/lib'")
-
-    # Extract changelog file
-    shelltools.system("gzip -dfq usr/share/doc/google-talkplugin/changelog.Debian.gz")
-
-    # Copying all needed stuff to /opt
-    pisitools.insinto("/opt/google/talkplugin", "opt/google/talkplugin/*")
-
-    # We don't need cron stuff
-    #pisitools.removeDir("/opt/google/talkplugin/cron")
-
-    # All doc files go to proper directory
-    pisitools.domove("/opt/google/talkplugin/attributions.txt", "/usr/share/doc/google-talkplugin")
-    pisitools.dodoc("usr/share/doc/google-talkplugin/changelog.Debian")
-
-    # I don't sure whether only these two symlinks enough
-    pisitools.dosym("/opt/google/talkplugin/libnpgoogletalk.so", "/usr/lib/browser-plugins/libnpgoogletalk.so")
-    pisitools.dosym("/opt/google/talkplugin/libnpgtpo3dautoplugin.so", "/usr/lib/browser-plugins/libnpgtpo3dautoplugin.so")
+    pisitools.insinto("/", "opt")
+    pisitools.insinto("/", "usr")
+    pisitools.removeDir("/opt/google/chrome-unstable")
+    pisitools.removeDir("/opt/google/chrome-beta")   
