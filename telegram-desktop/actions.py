40368113b69b64fd2cb1350576da4a16dca1eff6
Erkan IŞIK <erkanisik@yahoo.com>
2019-04-09 23:59:35 +0300

    Update actions.py

--- telegram-desktop/actions.py
+++ telegram-desktop/actions.py
@@ -4,10 +4,9 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
 shelltools.system("tar xvf tsetup.1.6.3.tar.xz")
