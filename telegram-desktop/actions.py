3f8e9ed87877f7df3ac6c8ab53f82ec6576b44fa
Erkan IŞIK <erkanisik@yahoo.com>
2019-04-10 12:23:41 +0300

    files update

--- telegram-desktop/actions.py
+++ telegram-desktop/actions.py
@@ -5,14 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 from pisi.actionsapi import get, pisitools, shelltools
 
-NoStrip = ["/opt", "/Telegram"]
-IgnoreAutodep = True
-
 def setup():
     shelltools.system("pwd")
     shelltools.system("tar xvf tsetup.1.6.3.tar.xz")
 
 def install():
-    pisitools.insinto("/opt/", "Telegram")
-    
-   
+    pisitools.insinto("/usr/bin/", "Telegram/*")  
