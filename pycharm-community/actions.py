5ac7f930ca94c1b4484c9e66e16f3c3c5cffe233
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2019-05-23 00:05:28 +0300

    pycharm:v.bump to 2019.1

--- pycharm-community/actions.py
+++ pycharm-community/actions.py
@@ -6,6 +6,5 @@ WorkDir = "."
 NoStrip = ["/"]
 
 def install():
-    #shutil.rmtree("pycharm-community-2016.2.3/jre")
-    pisitools.insinto("/opt/pycharm-community", "pycharm-community-2017.2.3/*")
+    pisitools.insinto("/opt/pycharm-community", "pycharm-community-2019.1.1/*")
     pisitools.dosym("/opt/pycharm-community/bin/pycharm.sh", "/usr/bin/pycharm-community")
