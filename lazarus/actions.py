8cf7c54a1bf40b30da6e13488118fd5799d35333
Sami BABAT <sami.babat@gmail.com>
2019-06-22 15:13:29 +0300

    update versiyon

--- lazarus/actions.py
+++ lazarus/actions.py
@@ -7,9 +7,9 @@ from pisi.actionsapi import shelltools, get, pisitools
 WorkDir = "."
 
 def install():
-    shelltools.system("rpm2targz lazarus-1.8.4-0.x86_64.rpm")
-    shelltools.system("tar -zxvf lazarus-1.8.4-0.x86_64.tar.gz")
+    shelltools.system("rpm2targz lazarus-2.0.2-0.x86_64.rpm")
+    shelltools.system("tar -zxvf lazarus-2.0.2-0.x86_64.tar.gz")
     pisitools.insinto("/", "usr")
     pisitools.insinto("/", "etc")
     shelltools.export("$THEPREFIX/usr/lib64/fpc/3.0.4/samplecfg", "$THEPREFIX/usr/lib64/fpc/3.0.4/ $ETCDIR")
-    shelltools.chmod("usr/lib64/lazarus/startlazarus", 0777)
+    # shelltools.chmod("usr/lib64/lazarus/startlazarus", 0777)
