b6c82159499ad1a344e639802ed0372f4cda7593
erkanışık1 <erkan@yahoo.com>
2019-08-28 22:22:56 +0300

    teamviewer version bump

--- teamviewer/actions.py
+++ teamviewer/actions.py
@@ -8,27 +8,12 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "."
-NoStrip = ["/opt/teamviewer/tv_bin/wine/drive_c/TeamViewer/tvwine.dll.so"]
+#WorkDir = "."
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf teamviewer_14.0.14470_amd64.deb")
-    shelltools.system("tar xf data.tar.xz")
-    
-def install():
-    pisitools.insinto("/opt/", "./opt/*")
-    pisitools.insinto("/etc/systemd/system", "./opt/teamviewer/tv_bin/script/teamviewerd.service")
-    
-    #necessary symlinks
-    pisitools.dosym("/opt/teamviewer/tv_bin/teamviewerd", "etc/init.d/teamviewerd")
-    pisitools.dosym("/opt/teamviewer/tv_bin/script/teamviewer", "usr/bin/teamviewer")
-    pisitools.dosym("/opt/teamviewer/logfiles", "var/log/teamviewer")
-    pisitools.dosym("/opt/teamviewer/config", "etc/teamviewer")
-    
-    pisitools.dodoc("%s/opt/teamviewer/doc/License.txt" %get.workDIR())
-    
-    
-    shelltools.chmod("%s/opt/teamviewer/doc/*" % get.installDIR(),0755)
-    shelltools.chmod("%s/opt/teamviewer/tv_bin/*" % get.installDIR(),0755)
+    shelltools.system("tar -xf teamviewer_14.5.1691_amd64.tar.xz")
 
+def install():
+    pisitools.insinto("~/teamviewer/", "teamviewer/*")
+    pisitools.dosym("~/teamviewer/tv_bin/TeamViewer", "/usr/bin/teamviewer")
