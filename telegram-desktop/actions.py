579f95479c18e9b8300954e0dd0e6fa228889820
erkanışık1 <erkan@yahoo.com>
2019-09-08 19:28:36 +0300

    version bump

--- telegram-desktop/actions.py
+++ telegram-desktop/actions.py
@@ -7,7 +7,7 @@ from pisi.actionsapi import get, pisitools, shelltools
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("tar xvf tsetup.1.8.3.tar.xz")
+    shelltools.system("tar xvf tsetup.1.8.4.tar.xz")
 
 def install():
     pisitools.insinto("/opt/telegram-desktop/", "Telegram/*")
