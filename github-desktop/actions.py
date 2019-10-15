e9eeb15e07b398338cf4c611881365664b0da417
Erkan IŞIK <erkanisik@yahoo.com>
2019-10-15 12:07:11 +0300

    github-desktop first release

---
+++ github-desktop/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+from pisi.actionsapi import get, pisitools, shelltools
+
+Version = get.srcVERSION()
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf GitHubDesktop-linux-"+Version+"-linux1.deb")
+    shelltools.system("tar xf data.tar.xz")
+    
+
+def install():
+    pisitools.insinto("/opt/github-desktop/", "opt/GitHub Desktop/*")
+    pisitools.dosym("/opt/github-desktop/github-desktop", "/usr/bin/github-desktop")
