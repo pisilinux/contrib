567f8df936527efa6c5cd5dd88f46ffbfeef0b08
Erkan IŞIK <erkanisik@yahoo.com>
2019-11-02 20:56:58 +0300

    verison bump

--- opera-beta/actions.py
+++ opera-beta/actions.py
@@ -14,21 +14,20 @@ Version = get.srcVERSION()
 
 def setup():
    # shelltools.system("pwd")
-    shelltools.system("ar xf opera-developer_%s_amd64.deb" %get.srcVERSION())
+    shelltools.system("ar xf opera-beta_%s_amd64.deb" %get.srcVERSION())
     shelltools.system("tar xvf data.tar.xz")
 
 def install():
     # root owns sandbox as it is setuid
-    shelltools.system("chown root:root usr/lib/x86_64-linux-gnu/opera-developer/opera_sandbox")
+    shelltools.system("chown root:root usr/lib/x86_64-linux-gnu/opera-beta/opera_sandbox")
     # ensure setuid
-    shelltools.system("chmod 4755 usr/lib/x86_64-linux-gnu/opera-developer/opera_sandbox")
+    shelltools.system("chmod 4755 usr/lib/x86_64-linux-gnu/opera-beta/opera_sandbox")
     pisitools.insinto("/", "usr")
 
     # Because ew.
     pisitools.move("%s/usr/lib/x86_64-linux-gnu/*" % get.installDIR(), "%s/usr/lib" % get.installDIR())
-    pisitools.dosym("/usr/lib/opera-developer/libffmpeg.so", "/usr/lib/libffmpeg.so")
     pisitools.removeDir("/usr/lib/x86_64-linux-gnu")
     pisitools.removeDir("/usr/share/lintian")
 
-    pisitools.remove("/usr/bin/opera-developer")
-    pisitools.insinto("/usr/bin/", "./usr/lib/x86_64-linux-gnu/opera-developer/opera-developer")
+    pisitools.remove("/usr/bin/opera-beta")
+    pisitools.insinto("/usr/bin/", "./usr/lib/x86_64-linux-gnu/opera-beta/opera-beta")
