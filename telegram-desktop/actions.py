62d14264cbc548a6635ef51e3d76cbcf5fedaac0
Erkan IŞIK <erkanisik@yahoo.com>
2020-01-01 16:53:54 +0300

    telegram version bump

--- telegram-desktop/actions.py
+++ telegram-desktop/actions.py
@@ -5,10 +5,13 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 from pisi.actionsapi import get, pisitools, shelltools
 
+
 def setup():
     shelltools.system("pwd")
-    shelltools.system("tar xvf tsetup.1.8.8.tar.xz")
+    shelltools.system("tar xvf tsetup.1.9.3.tar.xz")
+
 
 def install():
     pisitools.insinto("/opt/telegram-desktop/", "Telegram/*")
-    pisitools.dosym("/opt/telegram-desktop/Telegram", "/usr/bin/telegram-desktop")
+    pisitools.dosym("/opt/telegram-desktop/Telegram",
+                    "/usr/bin/telegram-desktop")
