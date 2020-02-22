59b7363c448ca2e75d7ad36fc48997aa694cb5c7
Erkan IŞIK <erkanisik@yahoo.com>
2020-02-22 12:47:14 +0300

    pisilife-2 klasör değişikliği
    eagle pakedi eklendi ardunino güncellendi fritzing elektronik altına eklendi

--- science/electronics/eagle/actions.py
+++ science/electronics/eagle/actions.py
@@ -3,6 +3,7 @@
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
+NoStrip = ["/opt/eagle-9.5.2"]
 
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
@@ -10,5 +11,5 @@ from pisi.actionsapi import shelltools
 
 
 def install():
-    pisitools.insinto("/usr/share/arduino-1.8.9", "*") 
-    pisitools.dosym("/usr/share/arduino-1.8.9/arduino", "/usr/bin/arduino")
+    pisitools.insinto("/opt/eagle-9.5.2", "*") 
+    pisitools.dosym("/opt/eagle-9.5.2/eagle", "/usr/bin/eagle")
