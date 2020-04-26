fe9fad8c1e64913dff5d9a76f048347f0aecee85
Erkan IŞIK <erkanisik@yahoo.com>
2020-04-26 17:32:31 +0300

    ramme version bump

--- ramme/actions.py
+++ ramme/actions.py
@@ -11,11 +11,11 @@ IgnoreAutodep = True
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf Ramme_3.2.3_amd64.deb")
+    shelltools.system("ar xf Ramme_3.2.5_amd64.deb")
     shelltools.system("tar xf data.tar.xz")
 
 def install():
     pisitools.insinto("/", "usr")
     pisitools.insinto("/", "opt")
     pisitools.dosym("/opt/Ramme/ramme", "/usr/bin/ramme")
-    pisitools.dosym("/usr/share/icons/hicolor/512x512/apps/ramme.png", "/usr/share/icons/hicolor/scalable/apps/ramme.png")
+    #pisitools.dosym("/usr/share/icons/hicolor/512x512/apps/ramme.png", "/usr/share/icons/hicolor/scalable/apps/ramme.png")
