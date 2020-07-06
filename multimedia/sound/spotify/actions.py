69f118e1cae097671abdd05ee5a211ca8b6d0973
Safa Arıman <safa@ariman.gen.tr>
2020-07-06 19:36:51 +0300

    some packages updated

--- multimedia/sound/spotify/actions.py
+++ multimedia/sound/spotify/actions.py
@@ -4,22 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import get, pisitools, shelltools
 
 NoStrip = ["/usr"]
 IgnoreAutodep = True
 
-Version = get.srcVERSION()
 
 def setup():
-    shelltools.system("pwd")
-    shelltools.system("ar xf spotify-client_%s.*_amd64.deb" % Version)
+    shelltools.system("ar xf spotify-client_%s.*_amd64.deb" % get.srcVERSION())
     shelltools.system("tar xf data.tar.gz")
+
 
 def install():
     pisitools.insinto("/", "usr")
     pisitools.dosym("/usr/lib/libcurl.so", "/usr/lib/libcurl-gnutls.so.4") 
-    #pisitools.dosym("/usr/share/spotify/spotify.desktop", "/usr/share/applications/spotify.desktop")
-    #pisitools.dosym("/usr/share/libffado/icons/hi64-apps-ffado.png", "/usr/share/pixmaps/ffado-mixer.png")
