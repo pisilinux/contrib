062b9dbd74b91e37b76b9ffba8127f0d9d2f1c8a
erkanisik1 <erkanisik@yahoo.com>
2020-07-06 13:49:29 +0300

    packages move

--- otter-browser/actions.py
+++ otter-browser/actions.py
@@ -4,17 +4,24 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
-from pisi.actionsapi import kde5
+#from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+#from pisi.actionsapi import kde5
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
 
 def setup():
-    kde5.configure("/")
+    #kde5.configure("/")
+    #shelltools.makedirs("build")
+    #shelltools.cd("build")
+    cmaketools.configure()
 
 def build():
-    kde5.make()
+    #kde5.make()
+     cmaketools.make()
 
 def install():
-    kde5.install()
+    #kde5.install()
+    cmaketools.install()
 
     pisitools.dodoc("COPYING", "CHANGELOG", "README.md")
