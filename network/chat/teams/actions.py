ab67b1e85e43d6522424101288803bfd62fd9599
erkanisik1 <erkanisik@yahoo.com>
2020-10-08 22:38:31 +0300

    new package teams
    sorunlu bir el atın :)

---
+++ network/chat/teams/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+from pisi.actionsapi import get, pisitools, shelltools
+
+
+
+def setup():
+   shelltools.system("ar xf teams_%s_amd64.deb" % get.srcVERSION())
+   shelltools.system("tar xf data.tar.xz")
+
+def install():
+	pisitools.insinto("/", "/usr")
