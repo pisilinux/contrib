ec926e8c755c582d890c970c8db43334e8257018
Erkan IŞIK <erkanisik@yahoo.com>
2020-11-01 18:38:08 +0300

    version bump
    anydesk sıkıntılı elden geçmesi gerekiyor.

--- development/pycharm-community/actions.py
+++ development/pycharm-community/actions.py
@@ -6,5 +6,5 @@ WorkDir = "."
 NoStrip = ["/"]
 
 def install():
-    pisitools.insinto("/opt/pycharm-community", "pycharm-community-2019.3.2/*")
+    pisitools.insinto("/opt/pycharm-community", "pycharm-community-2020.2.3/*")
     pisitools.dosym("/opt/pycharm-community/bin/pycharm.sh", "/usr/bin/pycharm-community")
