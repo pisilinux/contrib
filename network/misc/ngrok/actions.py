97db7d5f5671bffbfc9e408f43d4288dabf4bc86
erkanisik1 <erkanisik@yahoo.com>
2020-11-25 12:45:21 +0300

    ngrok new package

---
+++ network/misc/ngrok/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+from pisi.actionsapi import get, pisitools, shelltools
+
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("unzip ngrok-stable-linux-amd64.zip")
+   
+    
+
+def install():
+    pisitools.insinto("/opt/ngrok", "ngrok")
+    pisitools.dosym("/opt/ngrok/ngrok", "/usr/bin/ngrok")
