f2bdf4deda16766e7e05423e862934374f2141eb
erkanisik <erkanisik@yahoo.com>
2021-05-27 09:23:44 +0300

    version bump
    anydesk yüklenmesine rağmen açılmıyor elden geçmesi gerek

--- network/misc/anydesk/actions.py
+++ network/misc/anydesk/actions.py
@@ -9,11 +9,11 @@ from pisi.actionsapi import get, pisitools, shelltools
 NoStrip = ["/usr"]
 IgnoreAutodep = True
 
-Version = get.srcVERSION()
+
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf anydesk_6.0.1-1_amd64.deb")
+    shelltools.system("ar xf anydesk_%s_amd64.deb" % get.srcVERSION())
     shelltools.system("tar xf data.tar.gz")
 
 def install():
