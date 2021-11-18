c268f359e653764b7e80f08bfb5c05959d943fa8
erkanisik1 <erkanisik@yahoo.com>
2021-11-18 20:40:04 +0300

    new package FreeCAD

---
+++ multimedia/graphics/freecad/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt", "/usr"]
+IgnoreAutodep = True
+
+def install():
+    pisitools.dodir ("/opt/FreeCAD")
+    pisitools.doexe("OpenRA-Dune-2000-x86_64.AppImage", "/opt/FreeCAD")
+    pisitools.dosym("/opt/FreeCAD/FreeCAD_0.19-24291-Linux-Conda_glibc2.12-x86_64.AppImage", "/usr/bin/FreeCAD")
+   
