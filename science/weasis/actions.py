26cccb2ee552097b16d1474f733aaf165c7f05a8
alick01 <alicengizkurt@gmail.com>
2021-12-25 10:52:02 +0300

    New Package Weasis

---
+++ science/weasis/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt", "/usr"]
+IgnoreAutodep = True
+
+def install():
+    pisitools.dodir("/opt/weasis")
+    pisitools.dodir("/opt/weasis/bin")
+    shelltools.system("cp -r ../weasis/*  %s/opt/weasis" % get.installDIR())
+    shelltools.system("cp -r ../viewer-linux.sh  %s/opt/weasis/" % get.installDIR())
+    pisitools.dosym("/opt/weasis/viewer-linux.sh", "/usr/bin/weasis")
+   
