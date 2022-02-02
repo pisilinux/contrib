401e88253f67a552930cb54af41cd83a305c350b
alick01 <alicengizkurt@gmail.com>
2022-02-02 17:32:04 +0300

    Changed some files

--- jelvis/files/gmail.sh
+++ jelvis/files/gmail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-username="alicengizkurt@gmail.com"
-password="Alim**3694"
+username=""
+password=""
 echo
-evolution -u $username:$password --silent "https://mail.google.com/mail/feed/atom" |  grep -oPm1 "(?<=<title>)[^<]+" | sed '1d'
+google-chrome-stable -u $username:$password --silent "https://mail.google.com" |  grep -oPm1 "(?<=<title>)[^<]+" | sed '1d'
