0f82f628a666309efc4cc700b69142eae259731b
erkanisik1 <erkanisik@yahoo.com>
2022-04-03 21:36:26 +0300

    move package and verison bump
    Gitkraken move and version bump
    Anydesk version bump

--- network/misc/anydesk/actions.py
+++ network/misc/anydesk/actions.py
@@ -12,4 +12,6 @@ NoStrip = ["/"]
 def install():
     pisitools.dodir ("/opt/anydesk")
     pisitools.insinto("/opt/anydesk", "anydesk-6.1.1/*")
+    pisitools.insinto("/usr/share/polkit-1/actions", "anydesk-6.1.1/polkit-1/com.anydesk.anydesk.policy")
+    pisitools.insinto("/etc/init.d", "anydesk-6.1.1/init/anydesk")
     pisitools.dosym("/opt/anydesk/anydesk", "/usr/bin/anydesk")
