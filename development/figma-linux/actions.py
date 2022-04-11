cdf0c00803475c18dc99627f36b814623ddc7970
erkanisik1 <erkanisik@yahoo.com>
2022-04-11 23:43:37 +0300

    New package, version bump and package move

---
+++ development/figma-linux/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools, shelltools, get
+
+#WorkDir = "."
+#NoStrip = ["/"]
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("unzip figma-linux_0.9.6_linux_amd64.zip")
+
+def install():
+    #pisitools.dodir ("/opt/figma-linux")
+    pisitools.insinto("/opt/figma-linux", "*")
+    pisitools.dosym("/opt/figma-linux/figma-linux", "/usr/bin/figma-linux")
+    #pisitools.insinto("/usr/share/polkit-1/actions", "anydesk-6.1.1/polkit-1/com.anydesk.anydesk.policy")
+    #pisitools.insinto("/etc/init.d", "anydesk-6.1.1/init/anydesk")
