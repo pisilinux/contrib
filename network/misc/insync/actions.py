8ffe71bf263b15579ba432352990c45984cc6e3f
erkanisik1 <erkanisik@yahoo.com>
2022-05-01 18:42:52 +0300

    yeni kategori  ve yeni paketler eklendi
    insync bazı paketler güncellenince derlenecek bu haliyle derleniyor ama bizdeki paketler eski diyor.
    
    yandex-disk-indikator elden geçmesi gerek
    
    scratch-desktop yeni eklenen paket

---
+++ network/misc/insync/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+#NoStrip = ["/usr"]
+IgnoreAutodep = True
+
+Version = get.srcVERSION()
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf insync_"+Version+"-impish_amd64.deb")
+    shelltools.system("tar xf data.tar.gz")
+
+def install():
+    pisitools.insinto("/", "usr")
