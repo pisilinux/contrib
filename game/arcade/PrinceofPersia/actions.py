23f1c0bb15182c7739004757f951756aec32428f
erkanisik1 <erkanisik@yahoo.com>
2023-05-19 17:14:35 +0300

    new package Prince of Persia

---
+++ game/arcade/PrinceofPersia/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt", "/usr"]
+IgnoreAutodep = True
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("unzip SDLPoP-1.23.zip")
+
+def install():
+    pisitools.insinto( "/opt/", "SDLPoP-1.23/")
+   
