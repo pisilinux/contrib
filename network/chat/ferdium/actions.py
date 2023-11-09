1bd7039e30fa52b94109360cb248c6aa4c456318
erkanisik1 <erkanisik@yahoo.com>
2023-11-09 13:04:03 +0300

    new package ferdium

---
+++ network/chat/ferdium/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+WorkDir = "."
+NoStrip = ["/"]
+
+def install():
+    pisitools.dodir ("/opt/Ferdium")
+    pisitools.insinto("/opt/Ferdium", "Ferdium-linux-6.6.0/*")
+    pisitools.dosym("/opt/Ferdium/ferdium", "/usr/bin/ferdium")
+
+    
+   
