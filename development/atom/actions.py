a7d8d9d76727d9b18b7ca504ffa6a69c562564da
erkanisik1 <erkanisik@yahoo.com>
2024-01-16 18:02:49 +0300

    version bump

--- development/atom/actions.py
+++ development/atom/actions.py
@@ -3,6 +3,23 @@
 
 from pisi.actionsapi import get, pisitools, shelltools
 
+NoStrip = ["/usr"]
+IgnoreAutodep = True
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf atom-amd64.deb")
+    shelltools.system("tar xf data.tar.xz")
+
+def install():
+    pisitools.insinto("/", "usr")
+
+
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
 
 
 def setup():
@@ -11,3 +28,4 @@ def setup():
 
 def install():
     pisitools.insinto("/opt/atom/", "atom-1.57.0-amd64/*")
+"""
