c7153094a191cffbdfabe7b2602632e6c9af3690
erkanisik1 <erkanisik@yahoo.com>
2024-01-16 18:04:29 +0300

    update action.py

--- development/atom/actions.py
+++ development/atom/actions.py
@@ -13,19 +13,3 @@ def setup():
 
 def install():
     pisitools.insinto("/", "usr")
-
-
-"""sumary_line
-
-Keyword arguments:
-argument -- description
-Return: return_description
-
-
-def setup():
-    shelltools.system("pwd")
-    shelltools.system("tar xvf atom-amd64.tar.gz")
-
-def install():
-    pisitools.insinto("/opt/atom/", "atom-1.57.0-amd64/*")
-"""
